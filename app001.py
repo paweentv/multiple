9ba6f24ac0f635917d0f2a3807fd2b70a52717c4
Chanut <paweentv@gmail.com>
2021-04-25 14:54:17 +0700

    Update program can be input

--- app001.py
+++ app001.py
@@ -1,4 +1,5 @@
-for a in range(2, 21):
+x = input("โปรแกรมสูตรคูณ กรุณากรอกตัวเลขที่ต้องการ: ")
+for a in range(2, int(x)+1):
     for b in range(1, 13):
         result = a * b
         print(str(a) +"x"+ str(b) +"="+ str(result))
